273d5afa4849bc9551d0451bd3d061e2ba9fd156
Pierce <pierce@vulpes.dev>
2021-09-19 12:47:53 -0400

    add HUGE

--- lua/autorun/server/ttt_postcleanup_random_weapon_replacer.lua
+++ lua/autorun/server/ttt_postcleanup_random_weapon_replacer.lua
@@ -5,7 +5,8 @@ local weaponReplaceChances = {
     ["weapon_zm_mac10"] = 0.15,
     ["weapon_zm_revolver"] = 0.15, -- deagle
     ["weapon_ttt_glock"] = 0.15,
-    ["weapon_zm_pistol"] = 0.15 -- five seven
+    ["weapon_zm_pistol"] = 0.15, -- five seven
+    ["weapon_zm_sledge"] = 0.15 -- HUGE
 }
 
 hook.Add("PostCleanupMap",  "CFC_TTTWeaponReplacer_ReplaceWeaponsWithRandom", function()
